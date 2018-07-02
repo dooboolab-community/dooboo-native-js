c8368f4a7a03946ea89c542cf368fb4ba547b1f9
hyochan <dooboolab@gmail.com>
2018-07-02 20:33:27 +0900

    Changed name in the test components.

--- src/components/screen/__tests__/Intro.test.js
+++ src/components/screen/__tests__/Intro.test.js
@@ -1,22 +1,22 @@
 import 'react-native';
 import * as React from 'react';
-import Home from '../Home';
+import Intro from '../Intro';
 import appStore from '../../../stores/appStore';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import { shallow, render } from 'enzyme';
 
-describe('Home', () => {
+describe('Intro', () => {
   it('renders without crashing', () => {
-    const rendered = renderer.create(<Home store={appStore}/>).toJSON();
+    const rendered = renderer.create(<Intro store={appStore}/>).toJSON();
     expect(rendered).toMatchSnapshot();
     expect(rendered).toBeTruthy();
   });
 
   describe('component test', () => {
     const wrapper = shallow(
-      <Home store={appStore} />,
+      <Intro store={appStore} />,
     );
 
     it('renders as expected', () => {
