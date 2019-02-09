cffb4c95f31df1f5e5b11d6bd6f62916f3277dfa
hyochan <dooboolab@gmail.com>
2019-02-09 17:44:04 +0900

    Fixed not running test.

--- src/components/screen/__tests__/Intro.test.js
+++ src/components/screen/__tests__/Intro.test.js
@@ -6,6 +6,7 @@ import appStore from '../../../stores/appStore';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 const props = {
   store: appStore,
@@ -23,6 +24,12 @@ describe('Intro page DOM rendering test', () => {
     tree = renderer.create(component).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('shallow renders match snapshot', () => {
+    const shallow = new ShallowRenderer();
+    const result = shallow.render(component);
+    expect(result).toMatchSnapshot();
+  });
 });
 
 describe('Interaction', () => {
@@ -37,17 +44,18 @@ describe('Interaction', () => {
 
   it('Simulate onClick', () => {
     jest.useFakeTimers();
-    // const spy = jest.spyOn(instance.getInstance(), 'onLogin');
+    const spy = jest.spyOn(rendered.getInstance(), 'onLogin');
     const buttons = root.findAllByType(Button);
-    buttons[0].props.onPress();
+    root.instance.onLogin(); // == buttons[0].props.onPress();
     expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(root.instance.state.isLoggingIn).toEqual(true);
+
     jest.runAllTimers();
     expect(root.instance.state.isLoggingIn).toEqual(false);
     expect(props.store.user.displayName).toEqual('dooboolab');
     expect(props.store.user.age).toEqual(30);
     expect(props.store.user.job).toEqual('developer');
-    // expect(spy).toBeCalled();
+    expect(spy).toBeCalled();
     buttons[1].props.onPress();
     expect(props.navigation.navigate).toBeCalledWith('Temp');
   });
